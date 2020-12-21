50fdd5cea1c33b22e82ee12d8917691e7bbd4fd7
syf1234567 <1169662582@qq.com>
2020-12-21 22:03:53 +0800

    创建4期项目

---
+++ src/main/java/com/example/fourperiod/FourperiodApplication.java
@@ -0,0 +1,13 @@
+package com.example.fourperiod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourperiodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourperiodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fourperiod/FourperiodApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.fourperiod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FourperiodApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
